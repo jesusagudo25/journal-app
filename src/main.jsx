fcf1d443c7e83267c4040d996a1c77472d7eb9ce
Jesus Agudo <jagudo2514@gmail.com>
2025-05-28 22:12:09 -0500

    Add Redux store setup and authentication slice

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { JournalApp } from './JournalApp.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { JournalApp } from "./JournalApp.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./store";
+import { Provider } from 'react-redux'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter >
-      <JournalApp />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
+);
